160064b7995e71a36c4663ced0b95df8d7d48a0f
Gowsalya Nagaraj <gowsalyanagaraj07@gmail.com>
2025-08-24 23:51:35 +0530

    ADDED: Directory Navigator Files

---
+++ DirectoryNavigation/DirectioryNavigation.Test/Test1.cs
@@ -0,0 +1,73 @@
+﻿
+using DirectoryNavigation;
+
+namespace DirectioryNavigation.Test
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            DirectoryNavigator solution = new DirectoryNavigator();
+            string[] input = [
+                "cd /home",
+                "cd user",
+                "pwd",
+                "cd ..",
+                "pwd",
+                "cd ./projects/../code",
+                "pwd"
+                ];
+            string[] output = [
+                "/home/user/",
+                "/home/",
+                "/home/code/"
+                ];
+
+            // Act
+            string[] result = solution.Solve(input);
+
+            // Assert
+            for(int i = 0; i < output.Length; i++)
+            {
+                Assert.AreEqual(output[i], result[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Arrange
+            DirectoryNavigator solution = new DirectoryNavigator();
+            string[] input = [
+                "cd /",
+                "cd home",
+                "cd ./user//",
+                "cd ../..",
+                "cd ../..",
+                "cd var/log",
+                "pwd",
+                "cd /etc/./nginx/../ssh",
+                "pwd",
+                "cd ..",
+                "pwd"
+                ];
+            string[] output = [
+                "/var/log/",
+                "/etc/ssh/",
+                "/etc/"
+                ];
+
+            // Act
+            string[] result = solution.Solve(input);
+
+            // Assert
+            for (int i = 0; i < output.Length; i++)
+            {
+                Assert.AreEqual(output[i], result[i]);
+            }
+        }
+    }
+}
---
+++ DirectoryNavigation/DirectoryNavigation/DirectoryNavigator.cs
@@ -0,0 +1,89 @@
+﻿namespace DirectoryNavigation
+{
+    public class DirectoryNavigator
+    {
+        public List<string> currentPath = ["/"];
+        public int currentIndex = 1;
+
+        public string[] resultPwd;
+        public int resultIndex = 0;
+        public string[] Solve(string[] input)
+        {
+            resultPwd = new string[input.Count(x => x.Equals("pwd"))];
+            foreach (string s in input)
+            {
+                if(s.StartsWith("cd "))
+                {
+                    if(s.Substring(3).StartsWith("/") && currentPath.Count>0)
+                    {
+                        currentPath.RemoveRange(1, currentIndex - 1);
+                        currentIndex = 1;
+                        SplitDirectory(s.Substring(3));
+                    }
+                    else
+                    {
+                        SplitDirectory(s.Substring(3));
+                    }                    
+                }
+                else if(s.StartsWith("pwd"))
+                {
+                    FindPWD();
+                }
+            }
+            return resultPwd;
+        }
+
+        private void SplitDirectory(string path)
+        {
+            while(path.Length > 0)
+            {
+                if (path.StartsWith("//"))
+                {
+                    currentPath.Add("/");
+                    currentIndex++;
+                    path = path.Remove(0, 2);
+                }
+                else if (path.StartsWith("./"))
+                {
+                    path = path.Remove(0, 2);
+                }
+                else if (path.StartsWith("/"))
+                {
+                    path = path.Remove(0, 1);
+                }
+                else if(path.StartsWith("..") && currentIndex>1)
+                {
+                    currentIndex--;
+                    currentPath.RemoveAt(currentIndex);
+                    path = path.Remove(0, 2);
+                }
+                else if (path.StartsWith(".."))
+                {
+                    path = path.Remove(0, 2);
+                }
+                else if(path.Contains("/"))
+                {
+                    currentPath.Add(path.Substring(0, path.IndexOf('/'))+"/");
+                    path = path.Remove(0, path.IndexOf('/'));
+                    currentIndex++;
+                }
+                else
+                {
+                    currentPath.Add(path.Substring(0)+"/");
+                    path = path.Remove(0);
+                    currentIndex++;
+                }
+            }
+            
+        }
+
+        private void FindPWD()
+        {
+            foreach(var dir in currentPath)
+            {
+                resultPwd[resultIndex] += dir;
+            }
+            resultIndex++;
+        }
+    }
+}
